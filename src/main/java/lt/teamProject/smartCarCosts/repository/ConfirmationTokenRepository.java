81012c84c587d713234f33316f1d1242b07837d4
vlad <vladyslav.kosheliuk@student.ehu.lt>
2026-04-24 22:23:41 +0300

    Create main page

--- src/main/java/lt/teamProject/smartCarCosts/repository/ConfirmationTokenRepository.java
+++ src/main/java/lt/teamProject/smartCarCosts/repository/ConfirmationTokenRepository.java
@@ -1,9 +1,7 @@
 package lt.teamProject.smartCarCosts.repository;
 
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import lt.teamProject.smartCarCosts.entity.ConfirmationToken;
-import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.Optional;
 
