2ec5a8e36104f2be3d08ee8342adc2dfb87d0448
Vladyslav <vladyslav.kosheliuk@student.ehu.lt>
2026-05-18 10:38:20 +0300

    Delete logout

--- src/main/java/lt/teamProject/smartCarCosts/controller/SettingsController.java
+++ src/main/java/lt/teamProject/smartCarCosts/controller/SettingsController.java
@@ -66,10 +66,4 @@ public class SettingsController {
 
         return "redirect:/main-page";
     }
-
-    @GetMapping("/logout")
-    public String logout(HttpSession session) {
-        session.invalidate();
-        return "redirect:/login";
-    }
 }
